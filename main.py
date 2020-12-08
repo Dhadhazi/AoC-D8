357e2d3567716e18c8531a204a331a54f385845d
Dhadhazi <dhadhazi@gmail.com>
2020-12-08 09:01:26 +0100

    Part two done

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ acc = 0
 seen = {}
 i = 0
 repeat = False
-while not repeat:
+while not repeat or i <= len(data):
     command = readline(data[i])
     if i in seen:
         print(f"Repeat variable, acc is {acc}")
@@ -25,3 +25,32 @@ while not repeat:
         i += command["value"]
     else:
         i += 1
+
+# Part two
+def run_program(modified_data):
+    acc = 0;
+    i = 0;
+    t = 0;
+    while i < len(data) and t < 5000:
+        t += 1
+        command = readline(modified_data[i])
+        if command["type"] == "acc":
+            acc += command["value"]
+            i += 1
+        elif command["type"] == "jmp":
+            i += command["value"]
+        else:
+            i += 1
+    if i == len(data):
+        print(f"ACC was {acc}")
+
+
+for i in range(0, len(data)-1):
+    command = readline(data[i])
+    modified_data = data.copy()
+    if command["type"] == "nop":
+        modified_data[i] = f"jmp {command['value']}"
+        run_program(modified_data)
+    elif command["type"] == "jmp":
+        modified_data[i] = f"nop {command['value']}"
+        run_program(modified_data)
